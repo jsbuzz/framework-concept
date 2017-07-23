37b183562b11683f62ccc839b61ca266f621e648
Matyas Buczko <whybuzz@gmail.com>
2017-07-23 19:34:29 +0100

    fix debugger when handler is missing

--- flight/debugger.js
+++ flight/debugger.js
@@ -61,7 +61,10 @@ function handlerToString(handler) {
     if(handler.name) {
         return handler.name;
     }
-    return handler.toString().match(/_this[0-9][.]([^(]*)[(]/).pop();
+    return handler.length
+        ? handler.toString().match(/_this[0-9][.]([^(]*)[(]/).pop()
+        : 'Unknown'
+        ;
 }
 
 export default Debugger;
