c9e6892eb3efed6813c7d693cc94688951af557a
Matyas Buczko <whybuzz@gmail.com>
2017-08-06 22:30:28 -0400

    started test helpers

---
+++ flight/test.js
@@ -0,0 +1,9 @@
+
+Flight.Event.prototype.$$event = Flight.Event.prototype.event;
+Flight.Event.prototype.event = function() {
+    this.__proto__.lastTriggered = this;
+    return this.$$event();
+}
+Flight.Event.last = function() {
+    return this.prototype.lastTriggered;
+}
