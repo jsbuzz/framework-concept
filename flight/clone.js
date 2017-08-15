a654e095434f8d202de477434ecdf54861ceb414
Matyas Buczko <whybuzz@gmail.com>
2017-08-15 08:46:24 +0100

    working on clone

--- flight/clone.js
+++ flight/clone.js
@@ -1,13 +1,19 @@
 
 export default function clone(obj) {
-    if(!(obj instanceof Object)) {
+    if(!(obj instanceof Object) || obj instanceof Function) {
         return obj;
     }
     if(obj instanceof Array) {
         return obj.map( item => clone(item) );
     }
-    if(obj instanceof Number || obj instanceof String) {
+    if(
+		obj instanceof Number || obj instanceof String || obj instanceof Date ||
+		obj instanceof RegExp || obj instanceof Boolean
+	) {
         return new obj.constructor(obj);
+    }
+    if(obj.clone instanceof Function) {
+        return obj.clone();
     }
 
     const copied = {};
